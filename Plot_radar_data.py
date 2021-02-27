13cd7f42a6bbe2ec655844f0b0f1d5fd2ffae4ec
yuanhao3 <55822578+yuanhao3@users.noreply.github.com>
2021-02-27 21:26:35 +0800

    Update Plot_radar_data.py

--- Plot_radar_data.py
+++ Plot_radar_data.py
@@ -1,6 +1,7 @@
 """
 数据从雷达串口发出，由matlab接收，后由Python绘图并提取信息
 """
+from numpy import fft as nf
 from scipy.io import loadmat
 from matplotlib import pyplot as plt
 import os
@@ -36,28 +37,35 @@ def Load_data(filename):
     return radar_data
 
 
-def Data_plot(time_queue, all_datafile_name):
+def Data_plot(time_queue, fre_queue, all_datafile_name):
     for datafile_name in all_datafile_name:
         filename = filepath + '/' + datafile_name
 
         # 防止加载非.mat文件
         if filename[-1] == 't':
             radar_data = Load_data(filename)
+            # 由于直流分量的存在，导致0Hz处的幅值特别高需要排除
+            y_1 = abs(nf.fft(radar_data, len(time_queue)))[1: 121]
 
             # 绘图
-            # plt.ion()
-            plt.figure(figsize=(30, 15))
+            plt.ion()
+            plt.figure(figsize=(30, 20))
 
-            # 呼吸与心跳原始波形图
             if len(time_queue) == len(radar_data):
+                # 画图----信号时域图
+                plt.subplot(211)
                 plt.plot(time_queue, radar_data)
                 plt.title(datafile_name)
-                # plt.show()
+                # 画图----信号频域图
+                plt.subplot(212)
+                plt.plot(fre_queue, y_1)
+
+                plt.show()
+
                 dir_name = 'E:/yuanhao_python_work/Bio_Radar/Date_Process/Data_Plot/'
                 filename = dir_name + datafile_name + '.png'
-
                 plt.savefig(filename)
-                # plt.pause(1)
+                plt.pause(1)
                 print(filename)
         plt.close()
 
@@ -65,5 +73,6 @@ def Data_plot(time_queue, all_datafile_name):
 if __name__ == '__main__':
     filepath = input('复制粘贴文件夹地址: ')
     all_datafile_name = Readname(filepath)
-    time_queue = [i/102.4 for i in range(4096)]
-    Data_plot(time_queue, all_datafile_name)
+    time_queue = [i / 102.4 for i in range(4096)]
+    fre_queue = [i / 40 for i in range(1, 121)]
+    Data_plot(time_queue, fre_queue, all_datafile_name)
